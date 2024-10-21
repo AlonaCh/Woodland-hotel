56edf7974ffa610b1d7902854bd40c7024cd95d9
Alona Chubenko <aliona.chubenko@gmail.com>
2024-10-21 10:13:25 +0300

    Add app layout

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@
 
 // export default App
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import Bookings from "./pages/Bookings"
 import Cabins from "./pages/Cabins"
@@ -54,22 +54,31 @@ import Settings from "./pages/Settings"
 import Account from "./pages/Account"
 import Login from "./pages/Login"
 import PageNotFound from "./pages/PageNotFound"
+import GlobalStyles from "./styles/GlobalStyles"
+import AppLayout from "./ui/AppLayout"
 
 const App = () => {
   return (
+    <>
+    <GlobalStyles/>
     <BrowserRouter>
     <Routes>
-      <Route index element={<Dashboard/>}/>
+    <Route element={<AppLayout/>}>
+    <Route index element={<Navigate replace to='dashboard'/>}/>
     <Route path='dashboard' element={<Dashboard/>}/>
     <Route path='bookings' element={<Bookings/>}/>
     <Route path='cabins' element={<Cabins/>}/>
     <Route path='users' element={<Users/>}/>
     <Route path='settings' element={<Settings/>}/>
     <Route path='account' element={<Account/>}/>
+
+      </Route>
+      
     <Route path='login' element={<Login/>}/>
-    <Route path='pageNotFound' element={<PageNotFound/>}/>
+    <Route path='*' element={<PageNotFound/>}/>
     </Routes>
     </BrowserRouter>
+    </>
   )
 }
 
