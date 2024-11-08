adda99d9e8aedb8c6174ad52a01864bc31f6c2fd
Alona Chubenko <aliona.chubenko@gmail.com>
2024-11-08 10:01:21 +0200

    Add the compound component logic to AddCabin instead of state logic

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -3,22 +3,40 @@ import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
-
-const AddCabin = () => {
-    const [isOpenModal, setIsOpenModal] = useState(false);
-
-  return (
-    <div>
-
-      <Button onClick={()=> setIsOpenModal((show) => !show)}>
-        Add new cabin</Button>
-      {isOpenModal && (
-        <Modal onClose={()=> setIsOpenModal(false)}> 
-<CreateCabinForm onCloseModal={()=> setIsOpenModal(false)}/>
-        </Modal>
-        )}
-    </div>
-  )
+function AddCabin() {
+    //compounds the AddCabin component with the CreateCabinForm component
+    return <Modal>
+        <Modal.Open opens='cabin-form'> 
+        <Button>Add new cabin</Button>
+        </Modal.Open>
+            <Modal.Window name='cabin-form'>
+    <CreateCabinForm/>
+            </Modal.Window>
+    
+        {/* <Modal.Open opens='table'> 
+        <Button>Show table</Button>
+        </Modal.Open>
+            <Modal.Window name='table'>
+    <CreateCabinForm/>
+            </Modal.Window> */}
+    </Modal>
 }
+
+// const AddCabin = () => {
+//     const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div>
+
+//       <Button onClick={()=> setIsOpenModal((show) => !show)}>
+//         Add new cabin</Button>
+//       {isOpenModal && (
+//         <Modal onClick={()=> setIsOpenModal((show) => !show)}> 
+// <CreateCabinForm/>
+//         </Modal>
+//         )}
+//     </div>
+//   )
+// }
 
 export default AddCabin
