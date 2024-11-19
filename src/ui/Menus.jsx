e0dd10c14efb90ceb31e2fa81a7c5f1a4f35324a
Alona Chubenko <aliona.chubenko@gmail.com>
2024-11-19 12:02:44 +0200

    Position of the button list after clicking dots vertical

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -73,21 +73,27 @@ const MenusContext = createContext(); //we are gonna store info about menu
 function Menus({children}){
 //keep track which on is opened id
 const [openId, setOpenId] = useState(null);
+const [position, setPosition] = useState(null);
 //setter function
 const close = () => setOpenId(null);
 const open = setOpenId;
 
   return (
-    <MenusContext.Provider value={(openId, close, open)}>
+    <MenusContext.Provider value={{openId, close, open, position, setPosition}}>
       {children}
     </MenusContext.Provider>
   )
 }
 
 function Toggle({id}){
-const {openId, close, open} = useContext(MenusContext);
+const {openId, close, open, setPosition} = useContext(MenusContext);
 
-function handleClick(){
+function handleClick(e){
+  const rect = e.target.closest('button').getBoundingClientRect();
+  setPosition({
+    x:window.innerWidth - rect.width - rect.x,
+    y:rect.y + rect.height + 8,
+  })
   openId === '' || openId !== id ? open(id) : close()
 }
   return <StyledToggle onClick={handleClick}>
@@ -96,12 +102,12 @@ function handleClick(){
 }
 
 function List({id, children}){
-  const {openId} = useContext(MenusContext);
+  const {openId, position} = useContext(MenusContext);
 
   if(openId !== id) return null;
 
   return createPortal(
-    <StyledList position={{x: 18, y: 18}}>{children}</StyledList>, 
+    <StyledList position={position}>{children}</StyledList>, 
     document.body
   )
 }
