5622eb8ace180905ca22637db9bb60a742128c5c
Alona Chubenko <aliona.chubenko@gmail.com>
2024-11-28 14:58:04 +0200

    Add handle change function for sorting cabins

--- src/ui/Sort.jsx
+++ src/ui/Sort.jsx
@@ -1,8 +1,16 @@
+import { useSearchParams } from "react-router-dom";
 import Select from "./Select"
 
 const Sort = ({options}) => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    
+    function handleChange(e){
+        searchParams.set('sort', e.target.value)
+        setSearchParams(searchParams)
+    }
+
   return (
-    <Select options={options} type='white'/>
+    <Select options={options} type='white' onChange={handleChange}/>
   )
 }
 
